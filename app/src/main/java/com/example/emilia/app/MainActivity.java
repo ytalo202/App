36e7af5a2f989db3ca49dc83c41279e5095f6795
ytalo202 <1310375@utp.edu.pe>
2018-09-05 21:15:58 +0200

    Initial commit

---
+++ app/src/main/java/com/example/emilia/app/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.emilia.app;
+
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+//        ActionBar actionBar = getSupportActionBar();
+//        actionBar.hide();
+    }
+}
